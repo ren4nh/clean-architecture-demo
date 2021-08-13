0522c8adc83773cce232fd7e88183af7b121e14b
Renan Hartwig <renanhartwig@gmail.com>
2021-08-13 13:28:41 -0300

    Correção no docker-compose.yml

--- src/main/kotlin/br/com/hartwig/integration/products/core/interactor/create/CreateProductInteractorImpl.kt
+++ src/main/kotlin/br/com/hartwig/integration/products/core/interactor/create/CreateProductInteractorImpl.kt
@@ -1,11 +1,10 @@
 package br.com.hartwig.integration.products.core.interactor.create
 
 import br.com.hartwig.integration.products.core.dataprovider.ProductDataProvider
-import javax.inject.Named
 import javax.inject.Singleton
 
 @Singleton
-class CreateProductInteractorImpl(@Named("scylla") private val productDataProvider: ProductDataProvider) : CreateProductInteractor {
+class CreateProductInteractorImpl(private val productDataProvider: ProductDataProvider) : CreateProductInteractor {
 
     override fun create(
         request: CreateProductRequest
