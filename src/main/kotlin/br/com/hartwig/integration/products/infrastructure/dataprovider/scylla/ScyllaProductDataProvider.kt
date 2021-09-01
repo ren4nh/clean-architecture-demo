78fb6f07a5018782917786bffdaa34ae2ce889db
Renan Hartwig <renanhartwig@gmail.com>
2021-09-01 16:03:30 -0300

    Fix lint

--- src/main/kotlin/br/com/hartwig/integration/products/infrastructure/dataprovider/scylla/ScyllaProductDataProvider.kt
+++ src/main/kotlin/br/com/hartwig/integration/products/infrastructure/dataprovider/scylla/ScyllaProductDataProvider.kt
@@ -3,7 +3,6 @@ package br.com.hartwig.integration.products.infrastructure.dataprovider.scylla
 import br.com.hartwig.integration.products.core.dataprovider.ProductDataProvider
 import br.com.hartwig.integration.products.core.domain.Product
 import br.com.hartwig.integration.products.infrastructure.dataprovider.scylla.repository.ScyllaProductRepository
-import io.micronaut.context.annotation.Primary
 import java.util.UUID
 import javax.inject.Singleton
 
